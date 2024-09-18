fda760dd5904b09fa64fed48c6db958b781bcc1f
Aditya Rao <82911340+adityarao2005@users.noreply.github.com>
2024-09-18 03:45:47 +0000

    added spring security oauth2 resource server config

--- apps/backend/src/main/java/com/resumebuilder/backend/BackendApplication.java
+++ apps/backend/src/main/java/com/resumebuilder/backend/BackendApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BackendApplication {
 
 	public static void main(String[] args) {
+		// TODO: Update the JWT keys set and configuration wiht the service account from firebase
+		// TODO: Add test controllers to see if the spring security stuff is working or not
 		SpringApplication.run(BackendApplication.class, args);
 	}
 
---
+++ apps/backend/src/main/java/com/resumebuilder/backend/config/WebSecurityConfiguration.java
@@ -0,0 +1,16 @@
+package com.resumebuilder.backend.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class WebSecurityConfiguration {
+
+    protected SecurityFilterChain configure(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(authorize -> authorize.anyRequest().authenticated())
+                .oauth2ResourceServer((oauth2) -> oauth2.jwt(Customizer.withDefaults()));
+        return http.build();
+    }
+}
