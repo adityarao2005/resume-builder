55c8da6500282cfa01498bc79538309f97471c86
Aditya Rao <82911340+adityarao2005@users.noreply.github.com>
2024-09-18 02:49:47 +0000

    Initial Commit, fix werid glitch rendering

--- apps/frontend/src/components/resume/pdf-viewer.tsx
+++ apps/frontend/src/components/resume/pdf-viewer.tsx
@@ -18,6 +18,7 @@ export default function ResumeViewer() {
     const resumeState = useAppSelector((state) => state.resume);
     const [state, setState] = useState(resumeState);
     const ref = useRef<NodeJS.Timeout>();
+    const [key, setKey] = useState(0);
 
     // Add a delay/timeout to the state update to prevent iframe from reloading on every state change
     useEffect(() => {
@@ -27,14 +28,13 @@ export default function ResumeViewer() {
 
         ref.current = setTimeout(() => {
             setState(resumeState);
+            setKey(key + 1);
         }, 1000)
     }, [resumeState]);
 
     return (
-        <>
-            <PDFViewer className='flex-1'>
-                <RenderResumeDocument document={state} />
-            </PDFViewer>
-        </>
+        <PDFViewer key={key} className='flex-1'>
+            <RenderResumeDocument document={state} />
+        </PDFViewer>
     );
 }
