0a525b0869368f7e9cfa608d8d732727cc8146af
Aditya Rao <82911340+adityarao2005@users.noreply.github.com>
2024-09-24 04:32:02 +0000

    updated plan of action

--- apps/backend/src/main/java/com/resumebuilder/backend/controller/ResumeManagementController.java
+++ apps/backend/src/main/java/com/resumebuilder/backend/controller/ResumeManagementController.java
@@ -5,13 +5,17 @@ import org.springframework.web.bind.annotation.RestController;
 import com.resumebuilder.backend.models.resume.Resume;
 import com.resumebuilder.backend.service.ResumeService;
 
+import java.time.LocalDate;
 import java.util.List;
+import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 public class ResumeManagementController {
@@ -37,6 +41,17 @@ public class ResumeManagementController {
     public List<Resume> getResumeHistory(@PathVariable("id") String documentId) {
         // Get the history of a resume by ID
         return service.getResumeHistory(documentId, userDetails.getUsername());
+    }
+
+    @PostMapping("/resume")
+    public Resume createResume(@RequestBody Resume entity) {
+        // Set defaults for resume then persist it
+        entity.setDocumentId(UUID.randomUUID().toString());
+        entity.setVersion(ResumeService.INITIAL_VERSION);
+        entity.setUserId(userDetails.getUsername());
+        entity.setCreatedAt(LocalDate.now());
+        entity.setId(null);
+        return service.saveOrUpdateResume(entity);
     }
 
 }
--- apps/backend/src/test/java/com/resumebuilder/backend/BackendApplicationTests.java
+++ apps/backend/src/test/java/com/resumebuilder/backend/BackendApplicationTests.java
@@ -6,6 +6,11 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class BackendApplicationTests {
 
+	// TODO: make sure mongodb is connnecting and the server has startedand then
+	// perform the following
+	/*
+	 */
+
 	@Test
 	void contextLoads() {
 	}
